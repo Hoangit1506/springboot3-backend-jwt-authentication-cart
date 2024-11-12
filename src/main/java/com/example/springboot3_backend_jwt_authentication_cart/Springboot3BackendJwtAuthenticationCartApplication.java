dce238a34d344d5fc50a9be15a2765c16604f637
Hoangit1506 <hoangit1506@gmail.com>
2024-11-12 17:34:13 +0700

    Init commit

---
+++ src/main/java/com/example/springboot3_backend_jwt_authentication_cart/Springboot3BackendJwtAuthenticationCartApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot3_backend_jwt_authentication_cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3BackendJwtAuthenticationCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3BackendJwtAuthenticationCartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot3_backend_jwt_authentication_cart/Springboot3BackendJwtAuthenticationCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot3_backend_jwt_authentication_cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3BackendJwtAuthenticationCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
